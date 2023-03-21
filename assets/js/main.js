e6832036b95e3c80bd586cea6cb5ce5f1318f7eb
aure4 <aurebv@gmail.com>
2023-03-21 13:31:19 +0100

    skills columnas

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,3 +188,19 @@
 			});
 
 })(jQuery);
+
+const observer = new IntersectionObserver(entries => {
+	// Loop over the entries
+	entries.forEach(entry => {
+	  // If the element is visible
+	  if (entry.isIntersecting) {
+		// Add the animation class
+		entry.target.classList.add('animation');
+	  }
+	});
+  });
+  
+  observer.observe(document.querySelector('.progress-bar-1'));
+  observer.observe(document.querySelector('.progress-bar-2'));
+  observer.observe(document.querySelector('.progress-bar-3'));
+  
